a78e0effc10a48b62057a82a3958563d2ea295b3
Dev <joseph.dobrzanski@gmail.com>
2020-02-02 19:11:41 -0800

    quick fix

--- app/src/main/java/com/example/photoGallery/FilterActivity.java
+++ app/src/main/java/com/example/photoGallery/FilterActivity.java
@@ -1,5 +1,4 @@
 package com.example.photoGallery;
-<<<<<<< HEAD
 //
 import android.app.DatePickerDialog;
 import android.content.Intent;
@@ -15,7 +14,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
-=======
 
 import android.app.DatePickerDialog;
 import android.widget.DatePicker;
@@ -34,7 +32,6 @@ import android.widget.EditText;
 import android.util.Log;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
->>>>>>> 4111a2f2b6e2bdc698ff47148a7a2242a94928d7
 
 
 
@@ -42,10 +39,8 @@ public class FilterActivity extends AppCompatActivity {
 
     private TextView fromDate;
     private TextView toDate;
-<<<<<<< HEAD
     private EditText commentSearch;
 
-=======
     private EditText TRLocation;
     private EditText BLLocation;
     private EditText commentSearch;
@@ -54,7 +49,6 @@ public class FilterActivity extends AppCompatActivity {
     private Calendar toCalendar;
     private DatePickerDialog.OnDateSetListener fromListener;
     private DatePickerDialog.OnDateSetListener toListener;
->>>>>>> 4111a2f2b6e2bdc698ff47148a7a2242a94928d7
 
     private DatePickerDialog.OnDateSetListener mDateSetListener_from;
     private DatePickerDialog.OnDateSetListener mDateSetListener_to;
@@ -69,12 +63,9 @@ public class FilterActivity extends AppCompatActivity {
         toDate   = (TextView) findViewById(R.id.tvDate_to);
 
 
-<<<<<<< HEAD
 
-=======
         TRLocation = (EditText) findViewById(R.id.Location_TR);
         BLLocation   = (EditText) findViewById(R.id.Location_BL);
->>>>>>> 4111a2f2b6e2bdc698ff47148a7a2242a94928d7
         commentSearch = (EditText) findViewById(R.id.Comment_search);
 
         fromDate.setOnClickListener(new View.OnClickListener() {
@@ -138,13 +129,10 @@ public class FilterActivity extends AppCompatActivity {
     public void search(final View v) {
         Intent i = new Intent();
         i.putExtra("STARTDATE", fromDate.getText().toString());
-<<<<<<< HEAD
         i.putExtra("ENDDATE", toDate.getText().toString() + "_23_59_59");
-=======
         i.putExtra("ENDDATE", toDate.getText().toString());
         i.putExtra("STARTLOCATION", TRLocation.getText().toString());
         i.putExtra("ENDLOCATION", BLLocation.getText().toString());
->>>>>>> 4111a2f2b6e2bdc698ff47148a7a2242a94928d7
         i.putExtra("COMMENTSEARCH", commentSearch.getText().toString());
 
         setResult(RESULT_OK, i);
