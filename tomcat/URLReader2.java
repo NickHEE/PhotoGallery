2811ece33c73d957f8bab1548c7228a36ae2831a
NICKHEE <nick.huttemann@gmail.com>
2020-02-28 11:56:22 -0800

    tomcat

---
+++ tomcat/URLReader2.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+class SimpleThread extends Thread {
+	public SimpleThread(String str) {
+		super(str);
+	}
+	public void run() {
+
+           try {
+           URL oracle = new URL("http://localhost:8081/midp/hits");
+            BufferedReader in = new BufferedReader(
+            new InputStreamReader(oracle.openStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+              System.out.println(inputLine);
+            in.close();
+            } catch(Exception ex) { }
+        }
+}
+
+
+public class URLReader2 {
+    public static void main(String[] args)  {
+      int numDownloads = 2;
+      for (int i = 0; i < numDownloads; i++) {
+         new SimpleThread("download#"+i).start();
+      }      
+    }   
+}
