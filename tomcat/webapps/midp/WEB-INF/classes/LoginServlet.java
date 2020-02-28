e8ee74a1e965986c72fc54eb4c4711dd61d17b57
NICKHEE <nick.huttemann@gmail.com>
2020-02-28 15:58:41 -0800

    working DB login

--- tomcat/webapps/midp/WEB-INF/classes/LoginServlet.java
+++ tomcat/webapps/midp/WEB-INF/classes/LoginServlet.java
@@ -2,9 +2,16 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.sql.*;
 
 // Extend HttpServlet class
 public class LoginServlet extends HttpServlet {
+	
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/photogallery";
+  
+	static final String USERNAME = "root";
+	static final String PASS = "ilovebobgill69";
 
    // Method to handle GET method request.
    public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -15,13 +22,11 @@ public class LoginServlet extends HttpServlet {
 	
    }
 
-
-
    // Method to handle POST method request.
-   public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-	  String button = request.getParameter("button_press");
-	  
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      
+		String button = request.getParameter("button_press");
+
 		String username1 = "Nicholas";
 		String password1 = "root";
 		if ((username1.equals(request.getParameter("Username"))) && (password1.equals(request.getParameter("Password"))))
@@ -29,14 +34,36 @@ public class LoginServlet extends HttpServlet {
 		  // if username-password correct, display photo gallery things
 		  PrintWriter out = response.getWriter();
 		  out.print(1);
-		  //doGet(request, response);
 		  response.sendRedirect("/midp/PhotoGallery");
 		}
-		else 
-		{
-			// if username-password pair not recognized, go back to login form
-			response.setStatus(response.SC_MOVED_TEMPORARILY);
-			response.setHeader("Location", "http://localhost:8081/midp/login_form.html");   
+		try {
+			Connection conn = null;
+			Statement stmt = null;
+			
+			Class.forName(JDBC_DRIVER);
+			conn = DriverManager.getConnection(DB_URL, USERNAME, PASS);
+			stmt = conn.createStatement();
+			String query =  "SELECT EXISTS(SELECT * FROM users WHERE "+ "Username = \"%s\" AND " + "`Password` = \"%s\");";
+			
+			ResultSet rs = stmt.executeQuery(String.format(query, request.getParameter("Username"), request.getParameter("Password")));
+			
+			if (rs.isBeforeFirst()) {
+				response.sendRedirect("/midp/PhotoGallery");
+			}
+			else 
+			{
+				// if username-password pair not recognized, go back to login form
+				response.setStatus(response.SC_MOVED_TEMPORARILY);
+				response.setHeader("Location", "http://localhost:8081/midp/login_form.html");   
+			}
+			
+			stmt.close();
 		}
-	  }
+		catch (SQLException se) {
+			se.printStackTrace();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
